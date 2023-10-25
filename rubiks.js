ca852d79afd5251f734760acb3f935235a82f415
Paul Curcio <phc6515@rit.edu>
2023-10-25 16:16:20 -0400

    Updated rubiks.js to have interactive mouse controls

--- rubiks.js
+++ rubiks.js
@@ -1,33 +1,41 @@
 import * as THREE from 'three';
-// Create a scene
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
-
-// Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('canvas-container').appendChild(renderer.domElement);
+document.getElementById('cube-container').appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Green
-const cube = new THREE.Mesh(geometry, material);
+// Create a basic Rubik's Cube geometry
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterials = [
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Red
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // Green
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Blue
+    new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Yellow
+    new THREE.MeshBasicMaterial({ color: 0xffa500 }), // Orange
+    new THREE.MeshBasicMaterial({ color: 0xffffff })  // White
+];
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
 
-// Add the cube to the scene
 scene.add(cube);
+
+// Position the camera
+camera.position.z = 3;
+
+// Add interaction (rotate cube with mouse)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Create an animation loop
 const animate = () => {
     requestAnimationFrame(animate);
 
-    // Rotate the cube
+    // Rotate the cube (for demonstration purposes)
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    // Render the scene
     renderer.render(scene, camera);
 };
 
